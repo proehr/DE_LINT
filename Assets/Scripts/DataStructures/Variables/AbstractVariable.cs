91b0ec1713b0b532e253f8c1578d857f61341479
proehr <pauline.roehr@yahoo.de>
2022-02-20 12:23:49 +0100

    8-dev-gamecontroller: Add SO data structures, basic state machine, gamecontroller

---
+++ Assets/Scripts/DataStructures/Variables/AbstractVariable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace DataStructures.Variables
+{
+    [CreateAssetMenu(fileName = "AbstractVariable", menuName = "Data/Variables/AbstractVariable", order = 0)]
+    public class AbstractVariable<T> : ScriptableObject
+    {
+        public T value;
+    }
+}
