cb618e7d39421690a58422b7e6c9d1422241ae92
proehr <pauline.roehr@yahoo.de>
2022-02-27 16:43:54 +0100

    33-dev-cleanup-mechanic: add lint collection mechanic

---
+++ Assets/Scripts/LintCollection/Lint.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+namespace LintCollection
+{
+    public class Lint : MonoBehaviour
+    {
+        public enum LintType
+        {
+            SMALL,
+            NORMAL,
+            BIG
+        }
+
+        [SerializeField] private LintType lintType;
+        [SerializeField] private int lintValue;
+        [SerializeField] private LintInventory inventory;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                switch (lintType)
+                {
+                    case LintType.SMALL:
+                        inventory.AddSmallLint(lintValue);
+                        break;
+                    case LintType.NORMAL:
+                        inventory.AddNormalLint(lintValue);
+                        break;
+                    case LintType.BIG:
+                        inventory.AddBigLint(lintValue);
+                        break;
+                }
+
+                RemoveFromEnvironment();
+            }
+        }
+
+        private void RemoveFromEnvironment()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/LintCollection/LintInventory.cs
@@ -0,0 +1,70 @@
+﻿using DataStructures.Events;
+using DataStructures.Variables;
+using UnityEngine;
+
+namespace LintCollection
+{
+    [CreateAssetMenu(fileName = "LintInventory", menuName = "GameData/LintInventory", order = 0)]
+    public class LintInventory : ScriptableObject
+    {
+        [SerializeField] private int smallLintMaxValue;
+        [SerializeField] private IntVariable smallLintCount;
+        [SerializeField] private int normalLintMaxValue;
+        [SerializeField] private IntVariable normalLintCount;
+        [SerializeField] private int bigLintMaxValue;
+        [SerializeField] private IntVariable bigLintCount;
+        [SerializeField] private GameEvent onBigLintFull;
+        [SerializeField] private GameEvent onLintCountChanged;
+
+        public int SmallLintCount => smallLintCount.value;
+
+        public int NormalLintCount => normalLintCount.value;
+
+        public int BigLintCount => bigLintCount.value;
+
+        public void AddSmallLint(int value)
+        {
+            smallLintCount.value += value;
+            RecalcLintCounts();
+            onLintCountChanged.Raise();
+            CheckBigLint();
+        }
+
+        public void AddNormalLint(int value)
+        {
+            normalLintCount.value += value;
+            RecalcLintCounts();
+            onLintCountChanged.Raise();
+        }
+
+        public void AddBigLint(int value)
+        {
+            bigLintCount.value += value;
+            RecalcLintCounts();
+            onLintCountChanged.Raise();
+        }
+
+        private void RecalcLintCounts()
+        {
+            if (smallLintCount.value >= smallLintMaxValue)
+            {
+                smallLintCount.value = 0;
+                normalLintCount.value += 1;
+            }
+
+            if (normalLintCount.value >= normalLintMaxValue)
+            {
+                normalLintCount.value = 0;
+                bigLintCount.value += 1;
+            }
+        }
+        
+        private void CheckBigLint()
+        {
+            if (bigLintCount.value >= bigLintMaxValue)
+            {
+                onBigLintFull.Raise();
+            }
+        }
+    }
+}
