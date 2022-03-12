704b6ca84d4895f89582d62cc38b981be62b4337
proehr <pauline.roehr@yahoo.de>
2022-03-12 19:29:52 +0100

    52-dev-linta: swap to urp, add linta to player movement

---
+++ Assets/Scripts/Utility/Floaty.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Utility
+{
+    public class Floaty : MonoBehaviour
+    {
+        internal Vector3 startPos;
+    
+        void Start()
+        {
+            startPos = transform.position;
+        }
+
+        void Update()
+        {
+            transform.position = new Vector3(startPos.x, startPos.y + Mathf.Sin(Time.fixedTime * Mathf.PI * 0.4f)*0.05f, startPos.z);
+        }
+    }
+}
